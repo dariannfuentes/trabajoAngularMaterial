4bf292ebba3a6704f5c5ee800dbfd49ac8066ef6
darianfuentes <dariannfuentes@gmail.com>
2022-09-13 14:55:53 -0300

    1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { Componente4Component } from './componentes/componente4/componente4.component';
+import { Componente3Component } from './componentes/componente3/componente3.component';
+import { Componente2Component } from './componentes/componente2/componente2.component';
+import { Componente1Component } from './componentes/componente1/componente1.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"componente1",component:Componente1Component},
+  {path:"componente2",component:Componente2Component},
+  {path:"componente3",component:Componente3Component},
+  {path:"componente4",component:Componente4Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Componente1Component } from './componentes/componente1/componente1.component';
+import { Componente2Component } from './componentes/componente2/componente2.component';
+import { Componente3Component } from './componentes/componente3/componente3.component';
+import { Componente4Component } from './componentes/componente4/componente4.component';
+import { MenuComponent } from './menu/menu.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatBadgeModule } from '@angular/material/badge';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Componente1Component,
+    Componente2Component,
+    Componente3Component,
+    Componente4Component,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatBadgeModule,
+    MatFormFieldModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
